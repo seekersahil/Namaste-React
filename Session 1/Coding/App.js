2ad90035519d8ad646a1d6c2ef3fdc62752f0b3f
Sahil Gupta <seekersahil@gmail.com>
2022-12-25 10:07:13 +0530

    created nested elements

--- Session 1/Coding/App.js
+++ Session 1/Coding/App.js
@@ -1,6 +1,17 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
-let h1 = React.createElement("h1",
-    null
-,"Rendered with ReactJS");
-console.log(root)
-root.render(h1)
+let h1 = React.createElement("h1",{
+        style : {
+            fontSize:"25px",
+            color:"blue"
+        }
+    },"Rendered with ReactJS");
+let h2 = React.createElement("h2",{
+        style:{
+            fontSize:"20px",
+            color : "green"
+        }
+    },"Also rendered with ReactJS");
+let div =React.createElement("div",{
+    className : "container",
+},[h1,h2])
+root.render(div);
