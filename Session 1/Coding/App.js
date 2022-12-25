b1eee8a2283d7d5c4f729c604bb39aae77fb7301
Sahil Gupta <seekersahil@gmail.com>
2022-12-25 09:48:18 +0530

    Added DOM and html rendering

--- Session 1/Coding/App.js
+++ Session 1/Coding/App.js
@@ -1,6 +1,6 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let h1 = React.createElement("h1",
     null
-,"Rendered via react");
+,"Rendered with ReactJS");
 console.log(root)
 root.render(h1)
